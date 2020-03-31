8c5d2b77c928eaa3c41ccec0e05eff13ad21185b
Kisik Misik <700mmr@gmail.com>
2020-03-31 18:07:20 +0300

    added player control, artifical intelligence, collision

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let playing = true;
 let width = canvas.width;
 let height = canvas.height;
-let animationTime = 30;
+let animationTime = 20;
 let borderWidth = 15;
+let blockWidth = 80;
 
 //draw canvas and borders
 let drawCanvas = () => {
@@ -27,6 +29,15 @@ let circle = function (x, y, radius, isFilled) {
     }
 };
 
+let checkResult = function (message) {
+    playing = false;
+    ctx.font = "50px Courier";
+    ctx.baseLine = "middle";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.fillText(message, width / 2, height / 2);
+}
+
 // ball constructor
 class Ball {
     constructor(x, y) {
@@ -42,20 +53,46 @@ class Ball {
         circle(this.x, this.y, 7, true);
     }
 
+    checkBlockCollision() {
+        let isOutBottom = this.y >= height - 25; //25 is block height + padding
+        let isOutTop = this.y <= 25;
+        let isCollidedBottom = this.x >= player1.x - blockWidth / 2 && this.x <= player1.x + blockWidth / 2;
+        let isCollidedTop = this.x >= player2.x - blockWidth / 2 && this.x <= player2.x + blockWidth / 2;
+
+        // stop animation and show game over when ball out of canvas
+        if (this.y < 0) {
+            setTimeout(() => {checkResult("You Won")}, 200)   
+        } else if (this.y > height) {
+            setTimeout(() => {checkResult("Game Over")}, 200)
+        };
+
+        if ((isOutBottom && isCollidedBottom) || (isOutTop && isCollidedTop)) {
+            return true;
+        } else {
+            return false;
+        };
+    }
+
     move() {
         this.x += this.speed * this.xShift;
         this.y += this.speed * this.yShift;
 
         //checks collission with walls and player
-        if(this.y >= height - 25 || this.y <= 25) {
+        if(this.checkBlockCollision()) {
             this.yShift = -(this.yShift);
+            this.speed += 0.5;
         }
 
-        if(this.x > width - borderWidth || this.x === borderWidth) {
+        if(this.x >= width - borderWidth || this.x <= borderWidth) {
             this.xShift = -(this.xShift);
         }
     }
+
+    botMove() {
+        player2.x = this.x;
+    }
 };
+
 
 class Player {
     constructor(x, y) {
@@ -65,32 +102,40 @@ class Player {
 
     draw() {
         ctx.fillStyle = "white";
-        ctx.fillRect(this.x, this.y, 80, 20);
+        ctx.fillRect(this.x - blockWidth / 2, this.y, blockWidth, 20);
     }
 };
 
-let controls = {
-    37: "leftFirst",
-    39: "rightFirst",
-    65: "leftSecond",
-    68: "rightSecond"
+//update block position according to the mousemove event
+let updatePosition = mouse => {
+    let docWidth = window.innerWidth;
+    let currentMousePercent = mouse / docWidth * 100;
+    let oneProcentCanvas = width / 100;
+    let currentBlockPosition = oneProcentCanvas * currentMousePercent;
+    player1.x = currentBlockPosition;
+
 }
 
-document.addEventListener("keydown", (evt) => {
-    // job here
+// mousemove event handler
+document.addEventListener("mousemove", (evt) => {
+    updatePosition(evt.clientX);
 })
 
+// all animation process
 let ball = new Ball(width / 2, height / 2);
 let player1 = new Player(width / 2, height - 25);
 let player2 = new Player(width / 2, 5);
 let animation = () => {
-    ctx.clearRect(0, 0, width, height);
-    drawCanvas();
-    ball.draw();
-    ball.move()
-    player1.draw();
-    player2.draw();
-    setTimeout(animation, animationTime);
+    if (playing) {
+        ctx.clearRect(0, 0, width, height);
+        drawCanvas();
+        ball.draw();
+        ball.move();
+        ball.botMove();
+        player1.draw();
+        player2.draw();
+        setTimeout(animation, animationTime);
+    }  
 }
 
 animation();
